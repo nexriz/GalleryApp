f14a6196f14dcb25ce48605244e92f0c7ea41951
MrLott <nexriz@hotmail.com>
2017-03-13 13:42:56 +0100

    added animation to site

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
-  	const imgs = ['http://i.imgur.com/svfepd9.jpg', 'http://i.imgur.com/edguCVH.jpg', 'http://i.imgur.com/olaCAbx.jpg', 'http://i.imgur.com/1PLG7tr.jpg',
-  	 'http://i.imgur.com/X2R6YHw.jpg', 'http://i.imgur.com/WHldgcN.jpg', 'http://i.imgur.com/iBfJexO.jpg', 'http://i.imgur.com/kqzxNFY.jpg','http://i.imgur.com/714feUy.jpg']
+  	const imgs = ['http://i.imgur.com/svfepd9.jpg', 'http://i.imgur.com/edguCVH.jpg', 'http://i.imgur.com/olaCAbx.jpg', 
+  				  'http://i.imgur.com/1PLG7tr.jpg', 'http://i.imgur.com/X2R6YHw.jpg', 'http://i.imgur.com/WHldgcN.jpg', 
+  				  'http://i.imgur.com/iBfJexO.jpg', 'http://i.imgur.com/kqzxNFY.jpg', 'http://i.imgur.com/714feUy.jpg' ]
     return (
     	<Router>
 	    	<div className="container">
@@ -14,8 +15,13 @@ class App extends Component {
 	    			<h2>CL .<small>designs</small></h2>
 	    		</header>
 	    		<main>
-	    			<Route exact path="/" render={() => <ShowcaseSection title={'SHOWCASE'} small={'.designs'}/> }/>
-	    			<Route path="/About" render={() => <div style={{height: '800px'}}>About page</div> }/>
+	    			<Route exact 
+	    				   path="/" 
+	    				   render={() => <ShowcaseSection title={'SHOWCASE'} small={'.designs'}/> }
+	    			/>
+	    			<Route path="/About" 
+	    				   render={() => <div style={{height: '800px'}}>About page</div> }
+	    			/>
 
 	    			<GallerySection imgs={imgs}/>
 	    		</main>
@@ -29,10 +35,12 @@ class App extends Component {
 
 
 
+
+
 const NavBar = ({links}) => (
 	 <nav className="navbar">
 	 	<ul>
- 			{links.map((link, i) => <li key={i}><Link to={link === 'Home' ? '/' : link}>{link}</Link></li>)}
+ 			{links.map((link, i) => <li className={'animated fadeIn'} key={i}><Link to={link === 'Home' ? '/' : link}>{link}</Link></li>)}
    		</ul>
   	</nav>
 )
@@ -46,14 +54,14 @@ const NavBarMobile = () => (
 
 const ShowcaseSection = ({title, small}) => (
 	    <section className="showcase">
-   			<h1>{title}</h1>
-    		<small>{small}</small>
+   			<h1 className="animated fadeInUp titleCase">{title}</h1>
+    		<small className="animated fadeInUp smallCase">{small}</small>
     	</section>
 )
 
 const GallerySection = ({imgs}) => (
 		<section className="gallery">
-			{imgs.map((img, i) => <img className="painting" key={i} src={img} alt=""/>)}
+			{imgs.map((img, i) => <img className="animated zoomIn painting" key={i} src={img} alt=""/>)}
 		</section>
 	)
 
